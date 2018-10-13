170d0230ca1e15a299213425c544127ab164cff1
Nikhil T Nair <nikhiltnair@outlook.com>
2018-10-13 23:40:18 +0400

    update the script

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,6 +1,4 @@
 module.exports = {
   apiUrl: "http://localhost:7000/api/",
   baseUrl: "http://localhost"
-  //  /apiUrl: "http://shortener.muhzi.com/v1/api/",
-  // baseUrl: "http://muhzi.com"
 };
